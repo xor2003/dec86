38c7b668f782c6ee9071015668509041cc0908eb
xor <xor2003@gmx.com>
2024-06-08 23:20:14 +0200

    many

--- compare.py
+++ compare.py
@@ -112,7 +112,7 @@ def compare_instructions_impact(instruction: str):
     bytes32 = assembler(instruction, 32)
     simgr32 = prepare(arch_32, bytes32)
     print("~~16~~")
-    bytes16 = assembler(instruction, 16)[1:5]
+    bytes16 = assembler(instruction, 16)
     #bytes16=b"\xe9\xe7\x01"
     print(bytes16)
     simgr16 = prepare(arch_16, bytes16)
@@ -145,16 +145,51 @@ def compare_instructions_impact(instruction: str):
 """
 imul si,si,0x3 ; TODO cf, of
 imul si,si,0x1234 ; TODO cf, of
-jae 0x109  # TODO carry
-jb 0x106  # TODO carry
-jbe 0x109  # TODO carry
 jmp 0x1ea  # working. assember issue
 jmp -35  # working
 jmp 0xffffff35  # working
 shl dx,1  # flags correct
 shl dx,0  # TODO flags
+shl ax,1  # cf, of
 imul ax,ax,0x6  # TODO cf, of
+jno 0x106  # assembler
+jo 0x106
+jns 0x106
+js 0x106
+jnc 0x106
+jc 0x106
+jna 0x106
+ja 0x106
+jnl 0x106
+jl 0x106
+jng 0x106
+jg 0x106  # assembler
+shl bx,cl  # disagree
+shl si,cl  # disagree
+shr di,cl  # disagree
 
+call 0x17a # assembler
+call 0xfd92 # assembler
+callf [0x2e0:0xb38] # assembler
+
+pop bp  # ??
+pop di
+push ax
+push cs
+push si
+
+rol si,cl # TODO
+ror si,cl # TODO
+rcl si,cl # TODO
+rcr si,cl # TODO
+movsw # TODO
+idiv cx  # TODO
+in al,dx
+
+"""
+
+LIST="""
+int 0x21
 add ax,cx
 add bx,0x10
 add bx,dx
@@ -223,42 +258,12 @@ jnl 6
 jl 6
 jng 6
 jg 6
-"""
-
-LIST="""
-jno 0x106
-jo 0x106
-jns 0x106
-js 0x106
-jnc 0x106
-jc 0x106
-jna 0x106
-ja 0x106
-jnl 0x106
-jl 0x106
-jng 0x106
-jg 0x106
-
 neg cx
 nop
 or al,al
 or ax,ax
 or ch,0x80
 not cx
-shl ax,1
-
-shl bx,cl
-shl si,cl
-shr di,cl
-rol si,cl
-ror si,cl
-rcl si,cl
-rcr si,cl
-pop bp
-pop di
-push ax
-push cs
-push si
 ret
 retf
 sti
@@ -271,19 +276,13 @@ sub sp,0x34
 xchg bx,ax
 xor ah,ah
 xor bp,bp
-call 0x17a
-call 0xfd92
-callf [0x2e0:0xb38]
-int 0x21out dx,al
+sbb ax,ax
+sbb cx,cx
 
+out dx,al
 """
 
 CODE = """
-jz 5
-movsw
-idiv cx
-sbb ax,ax
-sbb cx,cx
 """
 for line in filter(None, LIST.splitlines()):
     result = compare_instructions_impact(line)
