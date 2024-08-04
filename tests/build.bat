984d4a3e4487653fa13b340583943c7539639ce4
xor <xor2003@gmx.com>
2024-08-05 00:21:29 +0200

    build separate functions for now

--- tests/build.bat
+++ tests/build.bat
@@ -5,10 +5,11 @@ set PATH=C:\TCC\BIN
 rem Extract the filename without extension from the parameter
 
 rem Compile the C file to an .obj file
-tcc -I%INCLUDE_PATH% -c -mt %1.c
-tcc -I%INCLUDE_PATH% -S -mt %1.c
+tcc -I%INCLUDE_PATH% -c -1 -mt %1.c
+tcc -I%INCLUDE_PATH% -S -1 -mt %1.c
 
 rem Link the .obj file to create a .com file
-tlink -t -L%LIB_PATH% %LIB_PATH%\c0t.obj %1.obj ,%1.com,%1.map,%LIB_PATH%\cs.lib
+#tlink -t -m -l -s -L%LIB_PATH% %LIB_PATH%\c0t.obj %1.obj ,%1.com,%1.map,%LIB_PATH%\cs.lib
+tlink -t -n -m -l -s %1.obj ,%1.bin,%1.map
 
 exit
