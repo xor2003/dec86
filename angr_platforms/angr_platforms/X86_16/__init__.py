f9b66d82f95db74d4a95e7c5ece46a193c165e57
xor2003 <>
2026-03-20 17:45:47 +0100

    Port x86-16 angr compatibility shims

--- angr_platforms/angr_platforms/X86_16/__init__.py
+++ angr_platforms/angr_platforms/X86_16/__init__.py
@@ -7,14 +7,20 @@ try:
     import angr.ailment as ailment
     from angr.calling_conventions import default_cc
     from angr.calling_conventions import SimComboArg, SimRegArg
+    from angr.analyses.calling_convention import utils as _cc_utils
     from angr.analyses.decompiler.return_maker import ReturnMaker
     from angr.analyses.decompiler.structured_codegen.c import CClosingObject, CConstant, CITE, CTypeCast, CUnaryOp
+    from angr.analyses.reaching_definitions import rd_state as _rd_state
+    from angr.analyses.variable_recovery import variable_recovery_base as _variable_recovery_base
     from angr.sim_type import SimTypeBottom, SimTypePointer
     from angr.sim_type import SimTypeChar, SimTypeFunction, SimTypeInt, SimTypeLong, SimTypeLongLong, SimTypeShort
     from angr.analyses.typehoon.simple_solver import BASE_LATTICES, BottomType, Int, Int16, TopType
     from angr.analyses.typehoon import simple_solver as _typehoon_simple_solver
     from angr.analyses.typehoon import translator as _typehoon_translator
-    from angr.analyses.typehoon import lifter as _typehoon_lifter
+    try:
+        from angr.analyses.typehoon import lifter as _typehoon_lifter
+    except ImportError:
+        _typehoon_lifter = None
     from angr.analyses.decompiler.clinic import Clinic
     from angr.analyses.typehoon.typeconsts import Pointer, Int16 as TCInt16
     from angr.knowledge_plugins.functions.function import Function
@@ -22,6 +28,39 @@ try:
     from angr.sim_variable import SimMemoryVariable
     from angr.sim_variable import SimStackVariable
     from .annotations import ANNOTATION_KEY
+
+    _orig_is_sane_register_variable = _cc_utils.is_sane_register_variable
+
+    def _is_sane_register_variable_8616(arch, reg_offset, reg_size, def_cc=None):
+        if arch.name == "86_16":
+            return True
+        return _orig_is_sane_register_variable(arch, reg_offset, reg_size, def_cc=def_cc)
+
+    if getattr(_cc_utils.is_sane_register_variable, "__name__", "") != "_is_sane_register_variable_8616":
+        _cc_utils.is_sane_register_variable = _is_sane_register_variable_8616
+
+    _orig_stack_addr_from_offset = _variable_recovery_base.VariableRecoveryStateBase.stack_addr_from_offset
+
+    def _stack_addr_from_offset_8616(self, offset):
+        if self.arch.bits == 16:
+            return (offset + 0x7FFE) & 0xFFFF
+        return _orig_stack_addr_from_offset(self, offset)
+
+    if (
+        getattr(_variable_recovery_base.VariableRecoveryStateBase.stack_addr_from_offset, "__name__", "")
+        != "_stack_addr_from_offset_8616"
+    ):
+        _variable_recovery_base.VariableRecoveryStateBase.stack_addr_from_offset = _stack_addr_from_offset_8616
+
+    _orig_initial_stack_pointer = _rd_state.ReachingDefinitionsState._initial_stack_pointer
+
+    def _initial_stack_pointer_8616(self):
+        if self.arch.bits == 16:
+            return claripy.BVS("stack_base", 16, explicit_name=True)
+        return _orig_initial_stack_pointer(self)
+
+    if getattr(_rd_state.ReachingDefinitionsState._initial_stack_pointer, "__name__", "") != "_initial_stack_pointer_8616":
+        _rd_state.ReachingDefinitionsState._initial_stack_pointer = _initial_stack_pointer_8616
 
     class Pointer16(Pointer, TCInt16):
         def __init__(self, basetype=None):
@@ -61,6 +100,70 @@ try:
 
     _typehoon_simple_solver.SimpleSolver._pointer_class = _pointer_class_16
 
+    _orig_simple_solver_init = _typehoon_simple_solver.SimpleSolver.__init__
+
+    def _simple_solver_init_8616(
+        self,
+        bits: int,
+        constraints,
+        typevars,
+        constraint_set_degradation_threshold: int = 150,
+        stackvar_max_sizes=None,
+    ):
+        if bits != 16:
+            return _orig_simple_solver_init(
+                self,
+                bits,
+                constraints,
+                typevars,
+                constraint_set_degradation_threshold=constraint_set_degradation_threshold,
+                stackvar_max_sizes=stackvar_max_sizes,
+            )
+
+        self.bits = bits
+        self._constraints = constraints
+        self._typevars = typevars
+        self.stackvar_max_sizes = stackvar_max_sizes if stackvar_max_sizes is not None else {}
+        self._constraint_set_degradation_threshold = constraint_set_degradation_threshold
+        self._base_lattice = BASE_LATTICES[bits]
+        self._base_lattice_inverted = networkx.DiGraph()
+        for src, dst in self._base_lattice.edges:
+            self._base_lattice_inverted.add_edge(dst, src)
+
+        self.processed_constraints_count = 0
+        self.simplified_constraints_count = 0
+        self.eqclass_constraints_count = []
+
+        self._equivalence = _typehoon_simple_solver.defaultdict(dict)
+        for func_tv in list(self._constraints):
+            if self._constraints[func_tv]:
+                self.processed_constraints_count += len(self._constraints[func_tv])
+                self.preprocess(func_tv)
+                self.simplified_constraints_count += len(self._constraints[func_tv])
+
+        self._repr_tv_to_tvs = _typehoon_simple_solver.defaultdict(set)
+        for tv, repr_tv in self._equivalence.items():
+            self._repr_tv_to_tvs[repr_tv].add(tv)
+
+        self.solution = {}
+        for tv, sol in self._equivalence.items():
+            if isinstance(tv, _typehoon_simple_solver.TypeVariable) and isinstance(
+                sol, _typehoon_simple_solver.TypeConstant
+            ):
+                self.solution[tv] = sol
+
+        self._solution_cache = {}
+        self.solve()
+        for func_tv in list(self._constraints):
+            self._convert_arrays(self._constraints[func_tv])
+
+        for tv, tv_eq in self._equivalence.items():
+            if tv not in self.solution and tv_eq in self.solution:
+                self.solution[tv] = self.solution[tv_eq]
+
+    if getattr(_typehoon_simple_solver.SimpleSolver.__init__, "__name__", "") != "_simple_solver_init_8616":
+        _typehoon_simple_solver.SimpleSolver.__init__ = _simple_solver_init_8616
+
     def _translate_pointer16(self, tc):
         if isinstance(tc.basetype, _typehoon_translator.typeconsts.BottomType):
             internal = SimTypeBottom(label="void").with_arch(self.arch)
@@ -83,25 +186,26 @@ try:
     _typehoon_translator.TypeConstHandlers[Pointer16] = _typehoon_translator.TypeTranslator._translate_Pointer16
     _typehoon_translator.SimTypeHandlers[SimTypePointer] = _typehoon_translator.TypeTranslator._translate_SimTypePointer
 
-    _orig_lifter_init = _typehoon_lifter.TypeLifter.__init__
+    if _typehoon_lifter is not None:
+        _orig_lifter_init = _typehoon_lifter.TypeLifter.__init__
 
-    def _typelifter_init_16(self, bits: int):
-        if bits not in (16, 32, 64):
-            raise ValueError("TypeLifter only supports 16-bit, 32-bit, or 64-bit pointers.")
-        self.bits = bits
-        self.memo = {}
+        def _typelifter_init_16(self, bits: int):
+            if bits not in (16, 32, 64):
+                raise ValueError("TypeLifter only supports 16-bit, 32-bit, or 64-bit pointers.")
+            self.bits = bits
+            self.memo = {}
 
-    def _lift_simtype_pointer_16(self, ty):
-        if self.bits == 16:
-            return Pointer16(self.lift(ty.pts_to))
-        if self.bits == 32:
-            return _typehoon_lifter.Pointer32(self.lift(ty.pts_to))
-        if self.bits == 64:
-            return _typehoon_lifter.Pointer64(self.lift(ty.pts_to))
-        raise ValueError(f"Unsupported bits {self.bits}.")
+        def _lift_simtype_pointer_16(self, ty):
+            if self.bits == 16:
+                return Pointer16(self.lift(ty.pts_to))
+            if self.bits == 32:
+                return _typehoon_lifter.Pointer32(self.lift(ty.pts_to))
+            if self.bits == 64:
+                return _typehoon_lifter.Pointer64(self.lift(ty.pts_to))
+            raise ValueError(f"Unsupported bits {self.bits}.")
 
-    _typehoon_lifter.TypeLifter.__init__ = _typelifter_init_16
-    _typehoon_lifter.TypeLifter._lift_SimTypePointer = _lift_simtype_pointer_16
+        _typehoon_lifter.TypeLifter.__init__ = _typelifter_init_16
+        _typehoon_lifter.TypeLifter._lift_SimTypePointer = _lift_simtype_pointer_16
 
     _orig_clinic_make_function_prototype = Clinic._make_function_prototype
 
@@ -333,13 +437,14 @@ try:
 
     def _combo_reg_expr(self, stmt, reg_arg):
         reg = self.arch.registers[reg_arg.reg_name]
+        ins_addr = getattr(stmt, "ins_addr", None)
         return ailment.Expr.Register(
             self._next_atom(),
             None,
             reg[0],
             reg_arg.size * self.arch.byte_width,
             reg_name=self.arch.translate_register_name(reg[0], reg_arg.size),
-            ins_addr=stmt.ins_addr,
+            ins_addr=ins_addr,
         )
 
     def _handle_return_8616(self, stmt_idx, stmt, block):
@@ -358,11 +463,12 @@ try:
                 ):
                     return _orig_returnmaker_handle_return(self, stmt_idx, stmt, block)
                 low_reg, high_reg = ret_val.locations
+                ins_addr = getattr(stmt, "ins_addr", None)
                 low = _combo_reg_expr(self, stmt, low_reg)
                 high = _combo_reg_expr(self, stmt, high_reg)
-                low_32 = ailment.Expr.Convert(self._next_atom(), low.bits, 32, False, low, ins_addr=stmt.ins_addr)
-                high_32 = ailment.Expr.Convert(self._next_atom(), high.bits, 32, False, high, ins_addr=stmt.ins_addr)
-                shift = ailment.Expr.Const(self._next_atom(), None, 16, 32, ins_addr=stmt.ins_addr)
+                low_32 = ailment.Expr.Convert(self._next_atom(), low.bits, 32, False, low, ins_addr=ins_addr)
+                high_32 = ailment.Expr.Convert(self._next_atom(), high.bits, 32, False, high, ins_addr=ins_addr)
+                shift = ailment.Expr.Const(self._next_atom(), None, 16, 32, ins_addr=ins_addr)
                 ret_expr = ailment.Expr.BinaryOp(
                     self._next_atom(),
                     "Or",
@@ -374,12 +480,12 @@ try:
                             [high_32, shift],
                             False,
                             bits=32,
-                            ins_addr=stmt.ins_addr,
+                            ins_addr=ins_addr,
                         ),
                     ],
                     False,
                     bits=32,
-                    ins_addr=stmt.ins_addr,
+                    ins_addr=ins_addr,
                 )
                 new_stmt = stmt.copy()
                 new_stmt.ret_exprs = [ret_expr]
